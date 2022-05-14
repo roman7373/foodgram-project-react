5b1fa877eb342447fd7300a02f144e5b94586e77
roman7373 <rra83ul@mail.ru>
2022-05-14 22:04:48 +0400

    server name

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -1,9 +1,13 @@
 import os
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-SECRET_KEY = 'n_&$-6!&us_9c1gvugx!o2!m$=tuuu3j+qcfd2mgnwws+qwt!#'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY', default='n_&$-6!&us_9c1gvugx!o2!m$=tuuu3j+qcfd2mgnwws+qwt!#')
 
 DEBUG = True
 
