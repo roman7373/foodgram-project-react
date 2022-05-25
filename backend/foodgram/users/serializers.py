7327ee60724f761fd2534833195062ffb02eef43
roman7373 <rra83ul@mail.ru>
2022-05-25 13:06:09 +0400

    djoser

--- backend/foodgram/users/serializers.py
+++ backend/foodgram/users/serializers.py
@@ -26,6 +26,27 @@ class UserSerializer(serializers.ModelSerializer):
             author=obj, user=user).exists()
 
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed'
+        )
+
+    def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return user.follower.filter(author=obj).exists()
+
+
 class CompactRecipeSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
     name = serializers.ReadOnlyField()
